5e9f28b679d2844b8f95dffd06875c3d2b877f4e
Can Yavuz <can@canundjan.de>
2015-06-17 14:43:21 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='broccoli',
+      version='0.1',
+      description='Bro Communication Library',
+      url='http://github.com/tschan/broccoli-package',
+      author='tschan',
+      author_email='tschan@bla.de',
+      license='MIT',
+      packages=['broccoli'],
+      zip_safe=False)
